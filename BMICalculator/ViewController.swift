6b20204c51d19e3e4f7e5f884f408a38c43b163a
GemaVNZ <gemavnz@gmail.com>
2024-07-23 13:58:00 +0200

    Se añaden los label de peso y altura con sus respectivos componentes. Se añade un text view para la descripción y un butón que calculará el IMC. Se deja la funcionalidad de la aplicación a falta de mejorar el diseño.

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var labelWeight: UILabel!
+    
+    @IBOutlet weak var labelHeight: UILabel!
+    
+    @IBOutlet weak var imcTextView: UITextView!
+    
+    var weight: Float = 80
+    var height: Int = 160
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func onWeightChangedSlider(_ sender: UISlider) {
+        weight = (sender.value)
+        labelWeight.text = "\(weight) KG"
+        updateIMCDescription()
+    }
+    
+    @IBAction func onHeightChangedStepper(_ sender: UIStepper) {
+        height =  Int (sender.value)
+        labelHeight.text = "\(height) cm"
+        updateIMCDescription()
+    }
+    
+    
+    @IBAction func IMCCalculateButton() {
+        updateIMCDescription()
+        }
+    
+    func updateIMCDescription() {
+        let result = weight / ((Float(height) / 100) * (Float(height) / 100))
+        var description: String
+        
+        switch result {
+        case 0.0..<18.5:
+            description = "Underweight"
+        case 18.5..<25:
+            description = "Normal weight"
+        case 25..<30:
+            description = "Overweight"
+        default:
+            description = "Obesity"
+        }
+        
+        imcTextView.text = "\(result) - \(description)"
+    }
 }
 
