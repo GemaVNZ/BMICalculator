1b03e3172c73a0d4c9cab27ce9f51c49737a6266
GemaVNZ <gemavnz@gmail.com>
2024-07-24 12:47:00 +0200

    Se mejora visualmente la aplicación y se añaden colores y formateo.

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -21,12 +21,14 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
     
     
     @IBAction func onWeightChangedSlider(_ sender: UISlider) {
         weight = (sender.value)
-        labelWeight.text = "\(weight) KG"
+        labelWeight.text = String(format: "%.2f KG", weight)
         updateIMCDescription()
     }
     
@@ -43,20 +45,39 @@ class HomeViewController: UIViewController {
     
     func updateIMCDescription() {
         let result = weight / ((Float(height) / 100) * (Float(height) / 100))
+        let formattedResult = String(format: "%.2f", result)
         var description: String
+        var color: UIColor
+        
+        let intenseYellow = UIColor(red: 1.0, green: 0.8, blue: 0.0, alpha: 1.0)
         
         switch result {
         case 0.0..<18.5:
             description = "Underweight"
+            color = UIColor.blue
         case 18.5..<25:
             description = "Normal weight"
+            color = UIColor.green
         case 25..<30:
             description = "Overweight"
+            color = intenseYellow
         default:
             description = "Obesity"
+            color = UIColor.red
         }
         
-        imcTextView.text = "\(result) - \(description)"
+        let attributedText = NSMutableAttributedString(string: "\(formattedResult)\n", attributes: [.font: UIFont.systemFont(ofSize: 32)])
+        
+        let descriptionText = NSAttributedString(string: description, attributes: [.foregroundColor: color, .font: UIFont.systemFont(ofSize: 24)])
+        attributedText.append(descriptionText)
+        
+        imcTextView.attributedText = attributedText
+        
+        imcTextView.textAlignment = .center
+        
+        //imcTextView.attributedText = attributedText
+        
+        //imcTextView.text = "\(formattedResult) - \(description)"
     }
 }
 
